8f3a2fc898eec9eda9fa70a9abc78a2ad6dea2c1
Jennifer Powell <jerpowel321@gmail.com>
2019-04-25 17:56:53 -0700

    update app.js file to show image cards

--- JonSnow-Clicky Game/src/App.js
+++ JonSnow-Clicky Game/src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
 import Nav from '../src/components/Nav';
+import friends from "./friends.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <div>
+        <Nav />
+      <Wrapper>
+          <h1>Lets Start the Game!</h1>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+      </div>
+    );
+  }
 }
 
 export default App;
