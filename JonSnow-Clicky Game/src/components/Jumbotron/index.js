ced121ab8471a259684d9e698384f73107ed31e4
Jennifer Powell <jerpowel321@gmail.com>
2019-04-25 23:43:52 -0700

    update navbar and jumbotron styling

--- JonSnow-Clicky Game/src/components/Jumbotron/index.js
+++ JonSnow-Clicky Game/src/components/Jumbotron/index.js
@@ -5,10 +5,10 @@ import "./style.css";
 function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid">
-            <div className="container">
-                <h1 className="display-4 text-white align-bottom text-center">Clicky Game!</h1>
-                <p className="lead text-white align-bottom text-center">Click on an image to earn points, but don't click on any more than once!</p>
-            </div>
+           
+                <h1 className="display-4 align-text-bottom text-center">Clicky Game!</h1>
+                <b><p className="lead align-text-bottom text-center">Click on an image to earn points, but don't click on any image more than once!</p></b>
+           
         </div>
     );
 }
