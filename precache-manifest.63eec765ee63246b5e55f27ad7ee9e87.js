e6b873f4a39c5019edc8bd2ed32a8cf104fd158f
Jennifer Powell <jerpowel321@gmail.com>
2019-04-26 10:15:28 -0700

    Updates

---
+++ precache-manifest.63eec765ee63246b5e55f27ad7ee9e87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7566537c4a7327dc145b6c0ddfe46153",
+    "url": "/JonSnow-ClickyGame/index.html"
+  },
+  {
+    "revision": "8e9128cc149460dcce96",
+    "url": "/JonSnow-ClickyGame/static/css/main.ff68c209.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/JonSnow-ClickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8e9128cc149460dcce96",
+    "url": "/JonSnow-ClickyGame/static/js/main.2b745d6f.chunk.js"
+  },
+  {
+    "revision": "d76e8d6a0afed4e91ce8",
+    "url": "/JonSnow-ClickyGame/static/js/runtime~main.899b3228.js"
+  }
+]);
